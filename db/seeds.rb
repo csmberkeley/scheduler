babe57fea80ebf28af60f1cd12eca253cd6a317b
Allan Tang (saasbook) <allan_tang@berkeley.edu>
2015-07-19 02:51:41 -0700

    Added admin seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin = User.create! :email => "admin@csm.com",
+             :password => "el psy congroo",
+             :password_confirmation => "el psy congroo"
+
+admin.admin = true
+admin.save
