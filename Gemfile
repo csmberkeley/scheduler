0e5337dc41b5ed1b8468380386f9e53f96b316f6
Allan Tang (saasbook) <allan_tang@berkeley.edu>
2015-07-19 01:04:57 -0700

    Installing devise

--- Gemfile
+++ Gemfile
@@ -38,3 +38,7 @@ gem 'spring',        group: :development
 # Use debugger
 # gem 'debugger', group: [:development, :test]
 
+
+#Extra gems added
+gem 'devise'
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+  #Set root to ensure devise works
+  root "home#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
