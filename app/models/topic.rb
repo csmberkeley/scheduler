f64a981e4f54c98077db296c7dffec108c8c4814
Allan Tang (saasbook) <allan_tang@berkeley.edu>
2015-07-19 17:55:00 -0700

    wrong syntax for many to many relation

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,4 @@
 class Topic < ActiveRecord::Base
   belongs_to :course
-  has_many_and_belongs_to :sessions, join_table: :sessions_topics
+  has_and_belongs_to_many :sessions, join_table: :sessions_topics
 end
