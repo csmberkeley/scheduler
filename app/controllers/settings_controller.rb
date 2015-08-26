29db6e892ec340d22a577df584984f8004e07174
Alex Zhang <alex.zhang@berkeley.edu>
2015-08-25 19:37:10 -0700

    add settings page to home page and beforefilter

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,10 +1,10 @@
 class SettingsController < ApplicationController
+    before_filter :check_logged_in, :check_admin
     def index
         @settings = Setting.all
 
     end
     def update
-        #Setting.find(params[:name]).toggle = params[:name]
         Setting.update(params[:setting].keys, params[:setting].values)
         redirect_to settings_path
         flash[:notice] = "Settings updated!"
