c481d4807a62e6f659ce9623420033598ebd151e
Alex Zhang <alex.zhang@berkeley.edu>
2015-09-06 21:36:50 -0700

    style section view

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -79,22 +79,7 @@ class AdminsController < ApplicationController
       @sections[course.course_name] = { "Monday" => [], "Tuesday" => [], "Wednesday" => [], 
         "Thursday" => [], "Friday" => [] }
       course.sections.each do | section |
-        number = section.name.split.last.to_i
-        day_of_the_week = number / 100 % 10
-        case day_of_the_week
-        when 0
-          @sections[course.course_name]["Monday"] << section
-        when 1
-          @sections[course.course_name]["Tuesday"] << section
-        when 2
-          @sections[course.course_name]["Wednesday"] << section
-        when 3
-          @sections[course.course_name]["Thursday"] << section
-        when 4
-          @sections[course.course_name]["Friday"] << section
-        else
-          next
-        end
+        @sections[course.course_name][section.getDay] << section
       end
     end
   end
