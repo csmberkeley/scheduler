27e18c798480d27917a5e2330e0a4c390b003860
Mike Aboody <mikeaboody@berkeley.edu>
2015-09-09 16:19:46 -0700

    Fixed some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,153 +16,58 @@ admin.save
 
 #mike's test data
 
+allan = User.create! :name => "Allan Tang",
+            :email => "allan_tang@berkeley.edu",
+            :password => "allanpass",
+            :password_confirmation => "allanpass",
+            :confirmed_at => "2015-09-09 02:50:19"
+
+
 mike = User.create! :name => "Mike Aboody",
 			:email => "mikeaboody@berkeley.edu",
             :password => "mikespass",
-            :password_confirmation => "mikespass"
-
-mike2 = User.create! :name => "Mike Aboody2",
-      :email => "mikeaboody@gmail.com",
-            :password => "mikespass",
-            :password_confirmation => "mikespass"
+            :password_confirmation => "mikespass",
+            :confirmed_at => "2015-09-09 02:50:19"
 
 cs61a = Course.create! :course_name => "CS61A",
 			:semester => "Fall",
 			:year => 2015,
 			:password => "pass",
-                  :description => "A course for computer science",
+                  :description => "Structure and Interpretation of Computer Programs",
                   :instructor => "John Denero"
 
 cs61b = Course.create! :course_name => "CS61B",
 			:semester => "Fall",
 			:year => 2015,
 			:password => "pass",
-                  :description => "A course for computer science2",
-                  :instructor => "Josh Hug"
+                  :description => "Data Structures",
+                  :instructor => "Paul Hillfinger"
 
+#cs61a
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+count = 0
+days.each do |d|
+      times = [10, 11, 12, 13, 14, 15, 16, 17]
+      times.each do |t|
+            Section.create! :name => "Section #{count}", 
+      :course_id => cs61a.id, :start => Time.new(2015,9,9, t,0,0, "+00:00"),:end => Time.new(2015,9,9, t+1,0,0, "+00:00"),
+      :date => d
+            count += 1
+      end 
+end
 
-
-mike_cs61a_enr = Enroll.create! :user_id => mike.id,
-			:course_id => cs61a.id
-mike2_cs61a_enr = Enroll.create! :user_id => mike2.id,
-      :course_id => cs61a.id
-
-mike.enrolls << mike_cs61a_enr
-mike2.enrolls << mike2_cs61a_enr
-
-mike_cs61b_enr = Enroll.create! :user_id => mike.id,
-			:course_id => cs61b.id
-mike2_cs61b_enr = Enroll.create! :user_id => mike2.id,
-      :course_id => cs61b.id
-
-mike.enrolls << mike_cs61b_enr
-mike2.enrolls << mike2_cs61b_enr
-
-mike_cs61a_section1 = Section.create! :name => "Section 1000", 
-      :course_id => cs61a.id, :empty => true,
-      :mentor => "Hue Hue", :location => "Soda 341A", :start => Time.new(2008,6,21, 13,30,0, "+09:00"),:end => Time.new(2008,6,21, 13,30,0, "+10:00")
-
-mike_cs61a_section2 = Section.create! :name => "Section 1100", 
-      :course_id => cs61a.id, :empty => true,
-      :mentor => "Randy Johnson", :location => "Soda 341B", :start => Time.new(2008,6,21, 13,30,0, "+10:00")
-
-mike_cs61a_section3 = Section.create! :name => "Section 1200", 
-      :course_id => cs61a.id, :empty => true,
-      :mentor => "George Patel", :location => "Soda 341C"
-
-mike_cs61b_section1 = Section.create! :name => "Section 2000", 
-      :course_id => cs61b.id, :empty => true,
-      :mentor => "Arangatang", :location => ""
-
-mike_cs61b_section2 = Section.create! :name => "Section 2100", 
-      :course_id => cs61b.id, :empty => true,
-      :mentor => "This My Name", :location => "Soda 341D"
-
-mike_cs61b_section3 = Section.create! :name => "Section 2200", 
-      :course_id => cs61b.id, :empty => true,
-      :mentor => "Alex Zhang", :location => "Soda 341E"
-
-Section.create! :name => "Section 1001", 
-      :course_id => cs61a.id, :empty => true,
-      :mentor => "Mike Aboody", :location => "Soda 341F"
-
-Section.create! :name => "Section 1002", 
-      :course_id => cs61a.id, :empty => true,
-      :mentor => "Mike Aboody 8", :location => "Soda 341H"
-
-# These actually don't save for some reason. -Allan
-# Removed direct relation between section and users. Use enrollment table. See section.rb
-# mike_cs61a_section1.users << mike
-# mike_cs61b_section1.users << mike
-
-# mike_cs61a_section2.users << mike2
-# mike_cs61b_section2.users << mike2
-
-mike_cs61a_section1.enrolls << mike_cs61a_enr
-mike_cs61b_section1.enrolls << mike_cs61b_enr
-
-mike_cs61a_section2.enrolls << mike2_cs61a_enr
-mike_cs61b_section2.enrolls << mike2_cs61b_enr
-
-mike_cs61a_offer = Offer.create! :body => "61A Section 1022 Offer", 
-      :section_id => mike_cs61a_section1.id,
-      :user_id => mike.id, 
-      :enroll_id => mike_cs61a_enr.id
-
-Want.create! :offer_id => mike_cs61a_offer.id,
-      :section_id => mike_cs61a_section2.id
-Want.create! :offer_id => mike_cs61a_offer.id,
-      :section_id => mike_cs61a_section3.id
-
-allan = User.create! :name => "Allan Tang",
-            :email => "allan_tang@berkeley.edu",
-            :password => "allanpass",
-            :password_confirmation => "allanpass"
-
-allan_cs61a_enr = Enroll.create! :user_id => allan.id,
-                  :course_id => cs61a.id
-
-allan.enrolls << allan_cs61a_enr
-
-allan2 = User.create! :name => "Allan Tang",
-            :email => "allan_tang2@berkeley.edu",
-            :password => "allanpass",
-            :password_confirmation => "allanpass"
-
-allan2_cs61a_enr = Enroll.create! :user_id => allan2.id,
-                  :course_id => cs61a.id
-
-allan2.enrolls << allan2_cs61a_enr
-
-allan3 = User.create! :name => "Allan Tang",
-            :email => "allan_tang3@berkeley.edu",
-            :password => "allanpass",
-            :password_confirmation => "allanpass"
-
-allan3_cs61a_enr = Enroll.create! :user_id => allan3.id,
-                  :course_id => cs61a.id
-
-allan3.enrolls << allan3_cs61a_enr
-
-allan4 = User.create! :name => "Allan Tang",
-            :email => "allan_tang4@berkeley.edu",
-            :password => "allanpass",
-            :password_confirmation => "allanpass"
-
-allan4_cs61a_enr = Enroll.create! :user_id => allan4.id,
-                  :course_id => cs61a.id
-
-allan4.enrolls << allan4_cs61a_enr
-
-allan5 = User.create! :name => "Allan Tang",
-            :email => "allan_tang5@berkeley.edu",
-            :password => "allanpass",
-            :password_confirmation => "allanpass"
-
-allan5_cs61a_enr = Enroll.create! :user_id => allan5.id,
-                  :course_id => cs61a.id
-
-allan5.enrolls << allan5_cs61a_enr
+#cs61b
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+count = 0
+days.each do |d|
+     times = [11, 12, 13, 14, 15]
+     times.each do |t|
+           Section.create! :name => "Section #{count}", 
+     :course_id => cs61a.id, :start => Time.new(2015,9,9, t,0,0, "+00:00"),:end => Time.new(2015,9,9, t+1,0,0, "+00:00"),
+     :date => d
+           count += 1
+     end 
+end
 
 Setting.create! :setting_name => "Enable Comments", :setting_type => "boolean", :value => "1", :name => "comments"
 
