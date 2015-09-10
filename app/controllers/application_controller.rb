d69719121399834fb1ccfdd2a185ed9e73070faa
Allan Tang (saasbook) <allan_tang@berkeley.edu>
2015-09-09 17:56:02 -0700

    Fixing naming

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) do |u|
       u.permit(:name, :email, :password, :password_confirmation)
     end
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:name, :email, :password, :password_confirmation, :current_password)
+    end
   end
 
   private
