19085c25c0ca1420da71f433106078a11899cbcd
Alex Zhang <alex.zhang@berkeley.edu>
2015-09-17 16:04:49 -0700

    fix mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,14 @@
 class UserMailer < ActionMailer::Base
   default from: "csmberkeley@gmail.com"
   def section_email(section) 
+    recipients = []
+    @section = section
     Enroll.where(section_id: section.id).each do |enroll| 
       @user = User.find(enroll.user_id)
-      @section = section
-      mail(to: @user.email, subject: "[CSM] Section Information")
+      recipients << @user.email
+    end
+    if recipients.size > 0
+      mail(to: recipients, subject: "[CSM] Section Information")
     end
   end
 end
