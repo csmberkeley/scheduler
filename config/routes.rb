4ba870eb3c9edf29e5a7edb42840fed7dd1b957e
Allan Tang <allan_tang@berkeley.edu>
2016-01-16 21:17:37 -0800

    +Added notice to check email after signing up by overriding devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   mount Ckeditor::Engine => '/ckeditor'
 
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "registrations" }
   #Set root to ensure devise works
   root "homes#index"
   #get "/courses" => "courses#index"
