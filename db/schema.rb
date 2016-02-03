93152c1bb9a905bc0449b30400427199d3f7b550
Alex Zhang <alex.zhang@berkeley.edu>
2016-02-02 23:00:26 -0800

    Add course specific base week and attendance count in manage section page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160113070150) do
+ActiveRecord::Schema.define(version: 20160203050505) do
 
   create_table "attendances", force: true do |t|
     t.integer  "enroll_id"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20160113070150) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "limit"
+    t.string   "base_week"
   end
 
   create_table "enrolls", force: true do |t|
