fd052ffd26579ff443ddd801ab7975b16ca2e9e4
Mason Scott <mscott2757@berkeley.edu>
2017-03-11 16:54:45 -0800

    added default disabled option for nil attendances

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -4,4 +4,8 @@ class Attendance < ActiveRecord::Base
 	belongs_to :senroll
 
 	enum status: [ :approved, :pending, :denied, :excused ]
+
+  def self.nil_statuses
+    Attendance.statuses.keys.to_a.unshift("Change attendance...")
+  end
 end
